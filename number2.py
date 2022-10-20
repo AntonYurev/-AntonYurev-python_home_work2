9ff95af2c38ec6406b61e8456d2e5d983f604d36
Anton <altay77@inbox.ru>
2022-10-20 23:55:08 +0700

    solution 3

--- number2.py
+++ number2.py
@@ -8,11 +8,23 @@
 # print(s)
 
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-n = int(input('Введите число : '))
-integral = 1
-integral_list = []
-for i in range (1, n+1):
-    integral = integral * i
-    integral_list.append(integral)
-print(integral_list)
+# n = int(input('Введите число : '))
+# integral = 1
+# integral_list = []
+# for i in range (1, n+1):
+#     integral = integral * i
+#     integral_list.append(integral)
+# print(integral_list)
+
+# Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
+# n = int(input('Введите число : '))
+# sum = 0
+# sum_list = []
+# for i in range(1, n+1):
+#     sum_list.append(f'{i} : ' + str(round((1 + 1 / i)**i, 2)) )
+# print(sum_list)
+
+
+    
+
 
